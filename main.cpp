71458c17df5e54f8840694001c41ff8120d8eb4e
KyrylOTheRylo <kiril.mosiychuk@gmail.com>
2021-09-16 14:13:57 +0300

    3 parts together

--- main.cpp
+++ main.cpp
@@ -39,7 +39,6 @@ unsigned long rand_first(int A, int C, int M )
 {   
     static unsigned long prev = 1;
     prev = (prev*A + C) % M  ;
-    cout << A << prev*A <<" " << C << endl;
     return prev;
 }
 unsigned long long  rand_second(int D, int A, int C, int M )
@@ -51,19 +50,24 @@ unsigned long long  rand_second(int D, int A, int C, int M )
 }
 unsigned long rand_third(int A, int C, int M)
 {
-    
-    static unsigned long prev = A;
-    prev = (prev + C) % M;
-    return prev;
+    static unsigned long prev = C, preprev = A ;
+    unsigned long temp;
+    temp = prev;
+    prev=(preprev + prev)% M;
+    preprev = temp;
+    return preprev;
 }
 void first(){
-    int  A = 3, C = 5, M = 12000;
+    int  A , C , M ;
     char check;
     
-    cout << "Insert the coefficient A and C and M( M >1000 ) if you want to get answer just put something except numbers" << endl;
+    cout << "write 'y' if you want to choose params by yourself" << endl;
     cin >> check;
-    if (check == 'y')  cout << "sdf" ;
-    cin >> A >> C >> M;
+    if (check == 'y') {
+        cout << "Insert the coefficient A and C and M( M >1000 )" << endl;
+        cin >> A >> C >> M ;
+        }
+    else{A = 3, C = 5, M = 12000;}
     for (int i = 0; i < M - 1; i++) { sort_(static_cast<float>(rand_first(A, C, M)) / static_cast<float>(M) * 100); }
     cout << "From 0 to 10 --- " << static_cast<float>(dig[0]) / static_cast<float>(M - 1) << endl;
     cout << "From 10 to 20 --- " << static_cast<float>(dig[1]) / static_cast<float>(M - 1) << endl;
@@ -79,11 +83,16 @@ void first(){
 
 }
 void second(){
-    int D=2, A=5, C=1231379, M=56321519;
+    int D, A, C, M ;
+    char  check;
 
-
-    cout << "Insert the coefficient D, A and C and M( M >1000 ) if you want to get answer just put something except numbers" << endl;
-    cin >> D >> A >> C >> M;
+    cout << "write 'y' if you want to choose params by yourself" << endl;
+    cin >> check;
+    if (check == 'y') {
+        cout << "Insert the coefficient D, A and C and M( M >1000 )" << endl;
+        cin >>D>> A >> C >> M ;
+    }
+    else D=2, A=5, C=1231379, M=56321519;
 
     for (int i = 0; i < M-1; i++) {sort_(static_cast<float>(rand_second(D, A, C, M)) / static_cast<float>(M) * 100);}
     cout  <<"From 0 to 10 --- " << static_cast<float>(dig[0])/ static_cast<float>(M-1)  <<  endl;
@@ -98,10 +107,15 @@ void second(){
     cout  <<"From 90 to 100 --- " << static_cast<float>(dig[9]) / static_cast<float>(M-1) <<  endl;
 }
 void third(){
-    int  A = 3, C = 5, M = 12000;
-
-    cout << "Insert the coefficient A and C and M( M >1000 ) if you want to get answer just put something except numbers" << endl;
-    cin >> A >> C >> M;
+    int  A , C , M ;
+    char check;
+    cout << "write 'y' if you want to choose params by yourself" << endl;
+    cin >> check;
+    if (check == 'y') {
+        cout << "Insert the coefficient A and C and M( M >1000 )" << endl;
+        cin >> A >> C >> M ;
+    }
+    else  A = 3, C = 5, M = 12000;;
     for (int i = 0; i < M - 1; i++) { sort_(static_cast<float>(rand_third(A, C, M)) / static_cast<float>(M) * 100); }
     cout << "From 0 to 10 --- " << static_cast<float>(dig[0]) / static_cast<float>(M - 1) << endl;
     cout << "From 10 to 20 --- " << static_cast<float>(dig[1]) / static_cast<float>(M - 1) << endl;
